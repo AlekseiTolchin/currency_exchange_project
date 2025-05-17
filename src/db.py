4e24ce5fb369b39d113f2ed888dd3434bb6e1f8f
Aleksey525 <alex_tolchin@mail.ru>
2025-05-17 16:45:11 +0300

    Initial commit

---
+++ src/db.py
@@ -0,0 +1,11 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+
+
+engine = create_async_engine("sqlite+aiosqlite:///./test.db")
+
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
+
+class Base(DeclarativeBase):
+    pass
---
+++ src/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get('/')
+def welcome() -> dict:
+    return {
+        'message': 'Currency exchange app'
+    }
